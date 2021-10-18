587f1a54f0daa8730db1f480b89b4eccb654397a
mugambi645 <mugambi645@gmail.com>
2021-10-18 13:54:04 +0300

    class user

--- src/app/profile-service/profile.service.ts
+++ src/app/profile-service/profile.service.ts
@@ -1,9 +1,13 @@
 import { Injectable } from '@angular/core';
-
+import { HttpClient } from '@angular/common/http';
+import { User } from '../user';
+import { resolve } from 'dns';
+import { environment } from 'src/environments/environment';
 @Injectable({
   providedIn: 'root'
 })
 export class ProfileService {
-  
-}
+
+  }
+
 
---
+++ src/app/user.ts
@@ -0,0 +1,12 @@
+export class User {
+    constructor (
+        public userName: string,
+        public user_followers: number,
+        public user_following: number,
+        public repositories: number,
+        public url: string,
+        public login: string
+
+
+    ) {}
+}
